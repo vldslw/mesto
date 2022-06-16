fd131955906c958ff8542428c98393aefa11c933
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-06-16 19:06:28 -0400

    feat: доделать popup

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__form');
+let popupName = popupForm.querySelector('.popup__name');
+let popupAbout = popupForm.querySelector('.popup__about');
+let editButton = document.querySelector('.profile__edit');
+let closeButton = document.querySelector('.popup__close-button');
+popupName.value = profileName.textContent;
+popupAbout.value = profileAbout.textContent;
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupAbout.value;
+  popup.classList.remove('popup_opened');
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+  popupName.value = profileName.textContent;
+  popupAbout.value = profileAbout.textContent;
+}
+
+closeButton.addEventListener ('click', closePopup);
+
