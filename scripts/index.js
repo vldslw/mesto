9477cf7b154658b59d6031e84b4d172ca7f0fd65
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-07-03 19:08:14 -0400

    feature: добавить карточки с фотографиями через js, исправить размеры картинок при изменении размера экрана

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,50 @@ popupForm.addEventListener('submit', submitForm);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener ('click', closePopup);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementTemplate = document.querySelector('.element-template').content;
+const elements = document.querySelector('.elements');
+
+const renderElements = () => {
+  initialCards.forEach(renderElement);
+};
+
+const renderElement = (object) => {
+  const htmlElement = elementTemplate.cloneNode(true);
+  htmlElement.querySelector('.element__title').textContent = object['name'];
+  htmlElement.querySelector('.element__photo').src = object['link'];
+  htmlElement.querySelector('.element__photo').alt = object['name'];
+
+  elements.append(htmlElement);
+
+};
+
+renderElements();
+
+
