f2fdc3dbef6a805ba397f7717ab3f2e620882d8a
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-07-03 22:06:12 -0400

    feat: изменить значения переменных и классов в попапе, который открывал редактирование профиля, добавить модификаторы к основному классу

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form');
-let popupName = popupForm.querySelector('.popup__input_type_name');
-let popupAbout = popupForm.querySelector('.popup__input_type_about');
-let editButton = document.querySelector('.profile__edit');
-let closeButton = document.querySelector('.popup__close-button');
+let profileEdit = document.querySelector('.profile__edit');
+let popupProfile = document.querySelector('.popup_type_profile');
+let popupProfileForm = popupProfile.querySelector('.popup__form');
+let popupProfileName = popupProfileForm.querySelector('.popup__input_type_name');
+let popupProfileAbout = popupProfileForm.querySelector('.popup__input_type_about');
+let popupProfileClose = document.querySelector('.popup__close-button');
 
-function openPopup () {
-  popup.classList.add('popup_opened');
-  popupName.value = profileName.textContent;
-  popupAbout.value = profileAbout.textContent;
+function openProfilePopup () {
+  popupProfile.classList.add('popup_opened');
+  popupProfileName.value = profileName.textContent;
+  popupProfileAbout.value = profileAbout.textContent;
 }
 
-function closePopup () {
-  popup.classList.remove('popup_opened');
+function closeProfilePopup () {
+  popupProfile.classList.remove('popup_opened');
 }
 
-function submitForm (evt) {
+function submitProfileForm (evt) {
   evt.preventDefault();
-  profileName.textContent = popupName.value;
-  profileAbout.textContent = popupAbout.value;
-  closePopup ();
+  profileName.textContent = popupProfileName.value;
+  profileAbout.textContent = popupProfileAbout.value;
+  closeProfilePopup ();
 }
 
-popupForm.addEventListener('submit', submitForm);
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener ('click', closePopup);
+popupProfileForm.addEventListener('submit', submitProfileForm);
+profileEdit.addEventListener('click', openProfilePopup);
+popupProfileClose.addEventListener ('click', closeProfilePopup);
 
 const initialCards = [
   {
