93cb11477b53e18406cb8f6b07f356ded1a0cacb
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-08-29 17:43:24 -0400

    feat: изменена файловая структура

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,24 @@
-import Card  from './Card.js';
-import FormValidator from './FormValidator.js';
-import Section  from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+import Card  from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section  from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
-// список названий и ссылок для первоначальных карточек
-const initialCards = [
-  {
-    title: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    title: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    title: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    title: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    title: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    title: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const profileName = '.profile__name';
-const profileAbout = '.profile__about';
-const profileEdit = document.querySelector('.profile__edit');
-
-const popupProfile = '.popup_type_profile';
-
-const pictureAddButton = document.querySelector('.profile__add');
-const popupPicture = '.popup_type_picture';
-
-const largePictureSelector = '.popup_type_largepicture';
-
-
-const cardTemplateSelector = '#element-template';
-const cardListSection = '.elements';
-
-const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__submit-button_invalid',
-  inputErrorClass: 'popup__input_error'
-};
-
-const formValidators = {};
+import {
+  initialCards,
+  profileName,
+  profileAbout,
+  profileEdit,
+  popupProfile,
+  pictureAddButton,
+  popupPicture,
+  largePictureSelector,
+  cardTemplateSelector,
+  cardListSection,
+  validationConfig,
+  formValidators
+} from '../utils/constants.js';
 
 // функция включения валидации
 const turnOnValidation = (config) => {
---
+++ utils/constants.js
@@ -0,0 +1,52 @@
+// список названий и ссылок для первоначальных карточек
+export const initialCards = [
+  {
+    title: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    title: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    title: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    title: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    title: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    title: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export const profileName = '.profile__name';
+export const profileAbout = '.profile__about';
+export const profileEdit = document.querySelector('.profile__edit');
+
+export const popupProfile = '.popup_type_profile';
+
+export const pictureAddButton = document.querySelector('.profile__add');
+export const popupPicture = '.popup_type_picture';
+
+export const largePictureSelector = '.popup_type_largepicture';
+
+export const cardTemplateSelector = '#element-template';
+export const cardListSection = '.elements';
+
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_invalid',
+  inputErrorClass: 'popup__input_error'
+};
+
+export const formValidators = {};
+
